23acb74c5d06210ca30b8b04bfb8235d98779e32
Alex Ermakov <alex.ermakov631@gmail.com>
2020-06-14 13:46:11 +0300

    DI with Constructor and with Referenc

--- src/main/java/org/example/SpringApp.java
+++ src/main/java/org/example/SpringApp.java
@@ -4,11 +4,21 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class SpringApp {
     public static void main(String[] args) {
+
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
                 "applicationContext.xml"
         );
+
         TestBean testBean = context.getBean("testBean", TestBean.class);
         System.out.println(testBean.getName());
+
+        Music music = context.getBean("classicalMusicBean", Music.class);
+        MusicPlayer musicPlayer = new MusicPlayer(music);
+        musicPlayer.playMusic();
+
+        MusicPlayer musicPlayer1 = context.getBean("musicPlayer", MusicPlayer.class);
+        musicPlayer1.playMusic();
+
         context.close();
     }
 }
